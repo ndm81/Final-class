bd70bf367436500aa463ba53dd93f124763ebcc4
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-04 13:07:35 +0530

    Add files via upload

---
+++ finalexm.java
@@ -0,0 +1,18 @@
+final  class Car{
+	void run(){
+		System.out.println("car is running");
+	}}
+
+
+ class Ferrari extends Car{
+       // void run(){
+ 		//	System.out.println("car is running");
+ 		 //}
+ 
+ 	   public static void main(String[] args) {
+
+ 		Ferrari fer=new Ferrari();
+ 		        fer.run();
+ 		        
+ 	}}
+ 
